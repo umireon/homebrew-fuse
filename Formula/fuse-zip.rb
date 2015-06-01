b9231b90422f1d304a800f595a84017684a0b492
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-01 03:15:11 +0100

    fuse-zip 0.4.0 (migration)

---
+++ Formula/fuse-zip.rb
@@ -0,0 +1,19 @@
+class FuseZip < Formula
+  desc "FUSE file system to create & manipulate ZIP archives"
+  homepage "https://code.google.com/p/fuse-zip/"
+  url "https://fuse-zip.googlecode.com/files/fuse-zip-0.4.0.tar.gz"
+  sha256 "db9eb13aa250061eaa7df6b1ee5022dbea54089094af94cc3d1767b63bdc9ca7"
+  head "https://code.google.com/p/fuse-zip/", :using => :hg
+
+  depends_on "pkg-config" => :build
+  depends_on "libzip"
+  depends_on :osxfuse
+
+  def install
+    system "make", "INSTALLPREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system bin/"fuse-zip", "--help"
+  end
+end
