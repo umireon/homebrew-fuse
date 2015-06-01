4f22c1082a4c1541c5d2654721644656f527ad85
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-01 03:18:15 +0100

    s3-backer 1.3.7 (migration)
    Closes #3.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/s3-backer.rb
@@ -0,0 +1,21 @@
+class S3Backer < Formula
+  desc "FUSE-based single file backing store via Amazon S3"
+  homepage "https://code.google.com/p/s3backer/"
+  url "https://s3backer.googlecode.com/files/s3backer-1.3.7.tar.gz"
+  sha256 "7a6f028d16301b0fd32679eebe34ff2d614f343979ee7a0fab0110b85934aaca"
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
+  depends_on :osxfuse
+
+  def install
+    inreplace "configure", "-lfuse", "-losxfuse"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/s3backer", "--version"
+  end
+end
