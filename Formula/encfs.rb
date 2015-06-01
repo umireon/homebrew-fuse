8903c13e2b6dc4b17d68e9b0f33f5dc80b99531f
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-01 03:14:20 +0100

    encfs 1.8.1 (migration)

---
+++ Formula/encfs.rb
@@ -0,0 +1,40 @@
+class Encfs < Formula
+  desc "Encrypted pass-through FUSE file system"
+  homepage "https://vgough.github.io/encfs/"
+  url "https://github.com/vgough/encfs/archive/v1.8.1.tar.gz"
+  sha256 "ed6b69d8aba06382ad01116bbce2e4ad49f8de85cdf4e2fab7ee4ac82af537e9"
+  head "https://github.com/vgough/encfs.git"
+
+  depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext" => :build
+  depends_on "boost"
+  depends_on "rlog"
+  depends_on "openssl"
+  depends_on "xz"
+  depends_on :osxfuse
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+    system "make", "-f", "Makefile.dist"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-boost=#{HOMEBREW_PREFIX}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    if Pathname.new("/Library/Filesystems/osxfusefs.fs/Support/load_osxfusefs").exist?
+      (testpath/"print-password").write("#!/bin/sh\necho password")
+      chmod 0755, testpath/"print-password"
+      system "yes | #{bin}/encfs --standard --extpass=#{testpath}/print-password #{testpath}/a #{testpath}/b"
+      system "umount", testpath/"b"
+    end
+  end
+end
