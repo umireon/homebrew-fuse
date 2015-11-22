1366b44a3183ccbb7989012f40965da06cdf7465
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-22 02:00:09 +0000

    ori: revision for boost

--- Formula/ori.rb
+++ Formula/ori.rb
@@ -3,6 +3,7 @@ class Ori < Formula
   homepage "http://ori.scs.stanford.edu/"
   url "https://bitbucket.org/orifs/ori/downloads/ori-0.8.1.tgz"
   sha256 "a6dd5677608c81d8cda657eb330661b5f9e0957a962a5588473d556ddf49f15f"
+  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "scons" => :build
