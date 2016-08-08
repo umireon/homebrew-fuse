059c761107f0d621f3d661e62cf3b1056d368591
ilovezfs <ilovezfs@icloud.com>
2016-08-08 04:51:30 -0700

    s3-backer 1.4.3
    Closes #55.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -1,21 +1,24 @@
 class S3Backer < Formula
   desc "FUSE-based single file backing store via Amazon S3"
   homepage "https://github.com/archiecobbs/s3backer"
-  url "https://s3backer.googlecode.com/files/s3backer-1.3.7.tar.gz"
-  sha256 "7a6f028d16301b0fd32679eebe34ff2d614f343979ee7a0fab0110b85934aaca"
+  url "https://github.com/archiecobbs/s3backer/archive/1.4.3.tar.gz"
+  sha256 "bf095e41b368067c766c0831e088e1e93411e29f4482efadaaf44e699ada16f6"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl"
   depends_on :osxfuse
 
   def install
+    system "./autogen.sh"
     inreplace "configure", "-lfuse", "-losxfuse"
-
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    system "#{bin}/s3backer", "--version"
+    system bin/"s3backer", "--version"
   end
 end
