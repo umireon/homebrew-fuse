af5d12269d61d87b9bdd6905cf1ec0875a6d81b8
ilovezfs <ilovezfs@icloud.com>
2016-08-27 00:51:21 -0700

    tup 0.7.5
    Closes #61.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -1,21 +1,20 @@
 class Tup < Formula
   desc "File-based build system"
   homepage "http://gittup.org/tup/"
+  url "https://github.com/gittup/tup/archive/v0.7.5.tar.gz"
+  sha256 "361b3e069308ce1d9505d1cb927999ac448811a3425c724123e0c48602a9d1e4"
   head "https://github.com/gittup/tup.git"
 
   depends_on "pkg-config" => :build
   depends_on :osxfuse
-
-  stable do
-    url "https://github.com/gittup/tup/archive/v0.7.4.tar.gz"
-    sha256 "e23f10b618925c85bc376ff0613dc4daf3990ecf2fc811ece54e87c7d64c9010"
-  end
 
   def install
     ENV["TUP_LABEL"] = version
     system "./build.sh"
     bin.install "build/tup"
     man1.install "tup.1"
+    doc.install (buildpath/"docs").children
+    pkgshare.install "contrib/syntax"
   end
 
   test do
