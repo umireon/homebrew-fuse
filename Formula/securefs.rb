9fc51d8a8506cd42d513f9a72dc32e2259f346b7
Siyuan Ren <netheril96@gmail.com>
2016-09-21 16:54:19 +0800

    securefs 0.5.4
    Update securefs to 0.5.4
    
    Closes #78.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -1,8 +1,8 @@
 class Securefs < Formula
   desc "Filesystem with transparent authenticated encryption"
   homepage "https://github.com/netheril96/securefs"
-  url "https://github.com/netheril96/securefs/archive/0.5.2.tar.gz"
-  sha256 "6feacc286d0b26279366806818d730114966f2106cc4cdb3bde5aef650650fe3"
+  url "https://github.com/netheril96/securefs/archive/0.5.4.tar.gz"
+  sha256 "ba69d000e135e600f4535adb7ec953fcd1b28faae7a7abe909a1b5f54eb4d2c1"
   head "https://github.com/netheril96/securefs.git"
 
   depends_on "cmake" => :build
