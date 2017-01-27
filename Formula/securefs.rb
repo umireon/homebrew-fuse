8c9b69f6883b5d9c03f612887b3514f65e50bcef
Siyuan Ren <netheril96@gmail.com>
2017-01-27 17:20:34 +0800

    securefs 0.6.0 (#99)

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -1,8 +1,8 @@
 class Securefs < Formula
   desc "Filesystem with transparent authenticated encryption"
   homepage "https://github.com/netheril96/securefs"
-  url "https://github.com/netheril96/securefs/archive/0.5.4.tar.gz"
-  sha256 "ba69d000e135e600f4535adb7ec953fcd1b28faae7a7abe909a1b5f54eb4d2c1"
+  url "https://github.com/netheril96/securefs/archive/0.6.0.tar.gz"
+  sha256 "698b8251640bd8b6ef04f94147a1c05aaa8495ebea4a03faa08ed91c967bd3cb"
   head "https://github.com/netheril96/securefs.git"
 
   depends_on "cmake" => :build
