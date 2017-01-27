13c3ae4ca4872694e695fb47b97d01bd8811ecc8
honggoff <honggoff@gmx.ch>
2017-01-27 10:20:03 +0100

    rofs-filtered 1.7 (#94)
    rofs-filtered changed the build system to cmake.

--- Formula/rofs-filtered.rb
+++ Formula/rofs-filtered.rb
@@ -1,27 +1,18 @@
 class RofsFiltered < Formula
   desc "Filtered read-only filesystem for FUSE"
   homepage "https://github.com/gburca/rofs-filtered/"
-  url "https://github.com/gburca/rofs-filtered/archive/rel-1.6.tar.gz"
-  sha256 "9f46269be24ba6ff23575c0a1a9a6c5923aab749e104984513884c668bf4bf90"
+  url "https://github.com/gburca/rofs-filtered/archive/rel-1.7.tar.gz"
+  sha256 "d66066dfd0274a2fb7b71dd929445377dd23100b9fa43e3888dbe3fc7e8228e8"
 
-  depends_on "pkg-config" => :build
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "intltool" => :build
-  depends_on "gettext" => :build
+  depends_on "cmake" => :build
   depends_on :osxfuse
   depends_on :macos => :yosemite
 
   def install
-    ENV.prepend "CPPFLAGS", "-I#{HOMEBREW_PREFIX}/include/osxfuse/fuse"
-    system "./autogen.sh"
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}",
-                          "--with-libfuse=#{HOMEBREW_PREFIX}"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}", *std_cmake_args
+      system "make", "install"
+    end
   end
 
   test do
